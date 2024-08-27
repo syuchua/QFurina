6260d47c9b7ea6aac768acfc8523647a96363112
yuchu <ma2102041310@outlook.com>
2024-08-27 16:44:26 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # main.py: 主程序入口
 import os, sys, asyncio, signal
 from app.logger import logger
-from app.config import Config
 from utils.boot import task, shutdown_handler
 from rich import print
 from rich.console import Console
@@ -26,12 +25,10 @@ rainbow_colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"
     
 if __name__ == '__main__':
     os.system('cls' if os.name == 'nt' else 'clear')  # 清空终端
-    sys.stdout.reconfigure(encoding='utf-8')  # 设置编码为 utf-8
+    #sys.stdout.reconfigure(encoding='utf-8')  # 设置编码为 utf-8
     for i, line in enumerate(asciiart.splitlines()):
         print(f"[{rainbow_colors[i % len(rainbow_colors)]}]{line}[/]")
     print("⭐️开源地址: https://github.com/syuchua/MY_QBOT\n")
-
-    config = Config.get_instance()
 
     # 注册信号处理程序
     signal.signal(signal.SIGINT, shutdown_handler)
